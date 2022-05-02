e5a5ec253dac94036e4d8dfb4e9920be8984a044
sumrak10 <nurlan.urazkulov@gmail.com>
2022-05-02 20:00:13 +0500

    4

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ import cherrypy
 import config
 
 WEBHOOK_HOST = '62.84.123.173'
-WEBHOOK_PORT = 8443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
-WEBHOOK_LISTEN = '62.84.123.173'  # На некоторых серверах придется указывать такой же IP, что и выше
+WEBHOOK_PORT = 443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
+WEBHOOK_LISTEN = '10.129.0.5'  # На некоторых серверах придется указывать такой же IP, что и выше
 
 WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Путь к сертификату
 WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Путь к приватному ключу
