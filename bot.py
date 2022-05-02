88c555b476be317e3b6dcde8f296b492a38722a3
sumrak10 <nurlan.urazkulov@gmail.com>
2022-05-02 19:42:02 +0500

    3

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import config
 
 WEBHOOK_HOST = '62.84.123.173'
 WEBHOOK_PORT = 443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
-WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
+WEBHOOK_LISTEN = '62.84.123.173'  # На некоторых серверах придется указывать такой же IP, что и выше
 
 WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Путь к сертификату
 WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Путь к приватному ключу
