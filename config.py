0028e2923b00065b4833c438f0026b63d9ab9406
sumrak10 <nurlan.urazkulov@gmail.com>
2022-05-02 21:11:38 +0500

    rp bot

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-token = "5348165766:AAHJ2NJwhg-p1qk0BHLQS63u3p-Nejq8Th8"
+# token = "5348165766:AAHJ2NJwhg-p1qk0BHLQS63u3p-Nejq8Th8"
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+token = '1506687912:AAF8VPByboL3t8vXGNK4k3D66SnW8B1pCuI'
+admin_id = '254407586'
